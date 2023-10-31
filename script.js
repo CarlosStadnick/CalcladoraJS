f885beb78671fc526af4e6085de534c5260782e0
CARLOS ALEXANDRE STADNICK <136737657+CarlosStadnick@users.noreply.github.com>
2023-10-30 21:09:41 -0300

    calcula

---
+++ script.js
@@ -0,0 +1,6 @@
+function somar(){
+    //pega valor digitado na caixa de texto dos valores 1,2
+    let num1 = parseFloat(document.getElementById('primeiroValor').value);
+
+    let num2 = parseFloat(document.getElementById('segundoValor').value);
+}

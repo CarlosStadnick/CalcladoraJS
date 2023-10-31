83ff647c9e545e3826d1f4bdc833515a275a03e9
CARLOS ALEXANDRE STADNICK <136737657+CarlosStadnick@users.noreply.github.com>
2023-10-30 21:24:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,30 @@ function somar(){
     let num1 = parseFloat(document.getElementById('primeiroValor').value);
 
     let num2 = parseFloat(document.getElementById('segundoValor').value);
+    //Adiciona conteúdo na tag P com ID resultado
+    document.getElementById('resultado').innerHTML = 'Resultado: ' +(num1 + num2)
+}
+function subtrair(){
+    //pega valor digitado na caixa de texto dos valores 1,2
+    let num1 = parseFloat(document.getElementById('primeiroValor').value);
+
+    let num2 = parseFloat(document.getElementById('segundoValor').value);
+    //Adiciona conteúdo na tag P com ID resultado
+    document.getElementById('resultado').innerHTML = 'Resultado: ' +(num1 - num2)
+}
+function multiplicar(){
+    //pega valor digitado na caixa de texto dos valores 1,2
+    let num1 = parseFloat(document.getElementById('primeiroValor').value);
+
+    let num2 = parseFloat(document.getElementById('segundoValor').value);
+    //Adiciona conteúdo na tag P com ID resultado
+    document.getElementById('resultado').innerHTML = 'Resultado: ' +(num1 * num2)
+}
+function dividir(){
+    //pega valor digitado na caixa de texto dos valores 1,2
+    let num1 = parseFloat(document.getElementById('primeiroValor').value);
+
+    let num2 = parseFloat(document.getElementById('segundoValor').value);
+    //Adiciona conteúdo na tag P com ID resultado
+    document.getElementById('resultado').innerHTML = 'Resultado: ' +(num1 / num2)
 }
